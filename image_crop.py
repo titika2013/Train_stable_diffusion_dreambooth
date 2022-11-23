aed29f04fdcf5e44392731e4847ad04ca60a062f
Grigoryan_tigran <titika2013@users.noreply.github.com>
2022-11-24 01:00:50 +0700

    Update image_crop.py

--- image_crop.py
+++ image_crop.py
@@ -5,18 +5,6 @@ import cv2
 from deepface import DeepFace
 import glob
 
-backends = [
-    'opencv',
-    'ssd',
-    'dlib',
-    'mtcnn',
-    'retinaface',
-    'mediapipe'
-]
-
-img_height = 512  # @param {type:"number"}
-img_width = 512  # @param {type:"number"}
-
 
 # # @markdown Where are your photos
 # source_photo_dir = "/content/gdrive/MyDrive/fresh_photos"  # @param {type:"string"}
@@ -36,7 +24,7 @@ img_width = 512  # @param {type:"number"}
 def resize(image, size):
     # If you are enlarging the image use INTER_CUBIC
     # If you are shrinking the image use INTER_AREA
-    height, width, channels = img.shape
+    height, width, channels = image.shape
     if height + width > sum(size):
         resized = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
     else:
@@ -46,15 +34,25 @@ def resize(image, size):
 
 #
 #
-def process(source_photo_dir="fresh_photos", dst_photo_dir="my_photos", size=(512, 512), face_detection=True,
+def process(source_photo_dir="fresh_photos", dst_photo_dir="my_photos", crop_size=512, face_detection=True,
             key_of_photo="MUYN"):
+    size = (crop_size, crop_size)
+    backends = [
+        'opencv',
+        'ssd',
+        'dlib',
+        'mtcnn',
+        'retinaface',
+        'mediapipe'
+    ]
     if not os.path.exists(dst_photo_dir):
         os.mkdir(dst_photo_dir)
     for count, item in enumerate(glob.iglob(os.path.join(source_photo_dir, "*"))):  # *.jpg
         img = cv2.imread(item)
         if face_detection:
-            face = DeepFace.detectFace(img_path=img, target_size=size,
-                                       detector_backend=backends[4], enforce_detection=False)
+            pass
+            face = DeepFace.detectFace(img_path=img, target_size=size, detector_backend=backends[4],
+                                       enforce_detection=False)
             if np.mean(face) < 1e-3:
                 continue
         resized_img = resize(img, size)
@@ -73,6 +71,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     image_path = args.image_path
     save_image_path = args.save_image_path
-    crop_size = (args.crop_size, args.crop_size)
+    crop_size = args.crop_size
     need_face_find = args.face_finder
-    process(image_path, image_path, save_image_path, crop_size, need_face_find)
+    process(image_path, save_image_path, crop_size, need_face_find)
